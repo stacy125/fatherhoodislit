b0bffa64216d50aa462e940b0f3b206b14000a74
stacy leitstein <unique523@yahoo.com>
2022-03-09 20:39:41 -0400

    making the necessary adjustments for update reactnavigation

--- App.js
+++ App.js
@@ -1,9 +1,11 @@
+import 'react-native-gesture-handler';
 import React from 'react';
 import { createAppContainer, createSwitchNavigator } from 'react-navigation';
 import { Provider as AuthProvider } from './src/context/EventContext';
 import { setNavigator } from './src/NavigationRef';
 import AuthNavigation from './src/navigation/AuthNavigation';
 import TabNavigator from './src/navigation/TabNavigator';
+import { NavigationContainer } from '@react-navigation/native';
 
 const switchNavigator = createSwitchNavigator({
   Auth: AuthNavigation,
@@ -15,9 +17,11 @@ const App = createAppContainer(switchNavigator)
 
 export default () => {
   return (
-    <AuthProvider>
-      <App ref={(navigator) => { setNavigator(navigator) }} />
-    </AuthProvider>
+    <NavigationContainer>
+      <AuthProvider>
+        <App ref={(navigator) => { setNavigator(navigator) }} />
+      </AuthProvider>
+    </NavigationContainer>
   );
 }
 
--- src/navigation/TabNavigator.js
+++ src/navigation/TabNavigator.js
@@ -1,35 +1,29 @@
-import { createTopTabNavigator } from 'react-navigation-stack';
 import AddEventScreen from '../screens/AddEventScreen';
 import EventScreen from '../screens/EventScreen';
 import HelpScreen from '../screens/HelpScreen';
 import ProfileScreen from '../screens/ProfileScreen';
-import SavedEvents from '../screens/SavedEvents';
+import SavedEventsScreen from '../screens/SavedEventsScreen';
 import HomeScreen from '../screens/HomeScreen';
+import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs';
 
-const TabNavigator = createTopTabNavigator({
-    Home: {
-        screen: HomeScreen
-    },
 
-    Profile: {
-        screen: ProfileScreen,
-    },
+const TabNavigator = () => {
 
-    Events: {
-        screen: EventScreen,
-    },
+    const Tab = createMaterialTopTabNavigator();
 
-    SavedEvents: {
-        screen: SavedEvents,
-    },
-
-    AddEvent: {
-        screen: AddEventScreen,
-    },
-    
-    Help: {
-        screen: HelpScreen,
+    function MyTabs() {
+        return (
+            <Tab.Navigator>
+                <Tab.Screen name="Home" component={HomeScreen} />
+                <Tab.Screen name="Profile" component={ProfileScreen} />
+                <Tab.Screen name="Event" component={EventScreen} />
+                <Tab.Screen name="AddEvent" component={AddEventScreen} />
+                <Tab.Screen name="SavedEvent" component={SavedEventsScreen} />
+                <Tab.Screen name="Help" component={HelpScreen} />
+            </Tab.Navigator>
+        );
     }
-})
+
+}
 
 export default TabNavigator;
